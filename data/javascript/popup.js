9c88864e259978852d7b0a43ce7ddede4b6dc4f8
OtomeSound <s890081tonyhsu@gmail.com>
2015-01-15 14:43:51 +0800

    Basic Plurk function test complete.
    1.	Local Storage Test
    2.	OAuth-io for plurk

--- data/javascript/popup.js
+++ data/javascript/popup.js
@@ -11,4 +11,41 @@ function nowTime() {
 		$('#time').text(time);
 		setTimeout(nowTime,1000);
 }
+
+function lastMsg() {
+	chrome.storage.local.get('test', function(res){
+		$('#lastMsg span').text(res.test);
+	});
+	return;
+}
+
+function saveMsg() {
+	var data = $('#testMsg').val();
+	if(!data){
+		return;
+	}else{
+		chrome.storage.local.set({'test': data}, function(){
+			alert('Message saved!');
+		});
+		lastMsg();
+	}
+}
+
 $(document).ready(nowTime);
+$(document).ready(lastMsg);
+$(document).ready(function(){
+	$('#saveMsg').bind('click', saveMsg);
+});
+
+OAuth.initialize("lSgIX_kBDPoIH1hwRtnOoIf5PeU");
+window.onload = function() {
+    document.getElementById('connect').addEventListener('click', function() {
+			OAuth.popup("plurk", function(e,r) {
+					console.log(e);
+					console.log(r);
+					r.get('/APP/Users/me').done(function(data) {
+						console.log(data);
+					});
+			});
+    });
+}
